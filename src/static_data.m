58bc458cc8d63c928fd520f37d393e35e9084822
niro7522 <niro7522@itll-lab-29a.itll.cu.int>
2015-04-08 11:57:18 -0600

    static data pull horz cat fix

--- src/static_data.m
+++ src/static_data.m
@@ -1,7 +1,10 @@
 % Static test report data pull and Isp calculation
+clear all
+close all
 
-
-global rho_air area_throat MassW0 gravity
+%establish constants
+gravity = 9.80665; % [m/s/s]
+MassW0 = 1; %[kg]
 
 % determine sample time
 sample_freq = 1.652; % [Hz]
@@ -17,18 +20,21 @@ end
 % calculates Isp for each data file using same algorithm as interp.m function
 s_data = [];
 for i = 1:length(files)
-    data_pull = data.files(i).name(:,3);
-    index = find(data_pull>2);
-    index2 = find(data_pull==min(data_pull));
+    data_pull(i).name = data.files(i).name(:,3);
+    index = find(data_pull(i).name>2);
+    index2 = find(data_pull(i).name==min(data_pull(i).name));
     index3 = index(1)-2:index2;
-    T_data = data_pull(index3); % [lbf]
-    count = length(T_data);
+    T_data(i).name = data_pull(i).name(index3); % [lbf]
+    count = length(T_data(i).name);
     ref_nom = zeros(count,1);
-    ref_offset = (linspace(0,min(data_pull),count))';
-    T_data_adj = (T_data - ref_offset)*4.44822162; % [N]
+    ref_offset = (linspace(0,min(data_pull(i).name),count))';
+    T_data_adj(i).name = (T_data(i).name - ref_offset)*4.44822162; % [N]
     x_sp = linspace(0,(count*sample_t),count);
-    I_sp(i) = trapz(x_sp,T_data_adj)./(gravity*MassW0); % [s]
-    s_data = [s_data T_data_adj];
+    I_sp(i) = trapz(x_sp,T_data_adj(i).name)./(gravity*MassW0); % [s]
+    x_axis = 1:1:count;
+    figure
+    plot(x_axis,T_data_adj(i).name)
 end
+
 
 
