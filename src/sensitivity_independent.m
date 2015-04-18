67bc027b192a7b97b363374c5be8b1693bdb817a
Aaron McCusker <aamc4303@colorado.edu>
2015-04-18 12:48:51 -0600

    Updated Sensitivity Scripts
    Launch Angle Variance is messed up though.... Will look at that more
    later.

--- src/sensitivity_independent.m
+++ src/sensitivity_independent.m
@@ -1,30 +1,28 @@
 %% James Pavek, Noel Puldon, Haoyu Li, Jake Harrell, Nick Monahan, Aaron McCusker
 %% ASEN2004 - Lab 2 - Simulated Bottle Rocket
-%% Sensitivity Function
+%% Main Function
 
-%% Purpose: Determine Sensitivity of Paramters that do not depend on static test stand data
+%% Purpose: Define constants and assess differences between models
+
+%% Constants
 
 % Reset Button
-close all; clear all; clc;
+clear all; close all; clc;
 
 % Start Stopwatch
 tic
 
 % Open Text File to Write Data Tables to
-fileID = fopen('Independent_Sensitivity_Data.txt','w');
+fileID = fopen('Sensitivity_Data.txt','a');
 
-%% Constants
+global velocity_windx velocity_windy velocity_windz pressure_ambient density_water volume_bottle discharge_coeff pressure_absolute gravity drag_coeff gas_constant volume_initial mass_air_initial pressure_end bottle_area throat_area temperature_initial density_air mass_rocket_initial launch_angle launch_rail_length mass_water_initial thrust_data friction_coefficient sample_freq
 
-global pressure_ambient density_water volume_bottle pressure_absolute gravity drag_coeff gas_constant volume_initial mass_air_initial pressure_end bottle_area throat_area temperature_initial density_air velocity_wind mass_rocket_initial pressure_absolute launch_angle launch_rail_length mass_water_initial test_data
-
-t_span = [0 10];
-ode_options = odeset('AbsTol',1e-7,'RelTol',1e-7);
 
 %% Material and Atmospheric Constants
-wind_speed = 0;
-wind_direction = 'NW';
-velocity_wind = windvector(wind_speed,wind_direction);
-velocity_wind = convvel(velocity_wind,'mph','m/s');
+velocity_wind = wind_vector(0,'W');                                                       % [m/s] Wind vector
+velocity_windx = velocity_wind(1);
+velocity_windy = velocity_wind(2);
+velocity_windz = velocity_wind(3);
 density_air = 1.042;                                                                      % [kg/m^3] Density of air
 gravity = -9.81;                                                                          % [m/s^2] Gravitation acceleration
 gas_constant = 287.15;                                                                    % [Specific gas constant of air]
@@ -34,30 +32,96 @@ temperature_initial = 300.0;                                                    
 velocity_initial = [0 0 0];                                                               % [m/s^2] Initial velocity
 bottle_mass = 0.15;                                                                       % [kg] Bottle mass
 launch_rail_length = 0.6;                                                                 % [m] Measured from cg to end of rail
-pos_initial = [0 0 0.1];                                                                  % [m] Initial position
-launch_angle = pi/4;                                                                      % [rad] Launch angle
+friction_coefficient = 0.0;                                                               % [N/A] Friction coefficient of each launch rods (mu_k)
 
 %% Bottle Dimensions and Weights
-drag_coeff = 0.3;                                                                         % [N/A] Drag coefficient of rocket
+drag_coeff = 0.2;                                                                         % [N/A] Drag coefficient of rocket
 discharge_coeff = .9;                                                                     % [N/A] Nozzle efficiency of the rocket
 volume_bottle = 0.002;                                                                    % [m^3] Volume of bottle
-volume_water_initial = 0.00101;                                                             % [m^3] Initial volume of water in bottle
+volume_water_initial = 0.001;                                                             % [m^3] Initial volume of water in bottle
 
 pressure_gage = 40.0;                                                                     % [psi] Initial gauge pressure of air in bottle
 pressure_gage = convpres(pressure_gage,'psi','Pa');
 pressure_absolute = pressure_gage+pressure_ambient;                                       % [Pa] Intial absolute pressure of air in bottle
 
 bottle_diameter=.109;                                                                     % [m] Diameter of bottle
-bottle_area = (pi*bottle_diameter^2);
+bottle_area = (pi*(bottle_diameter/2)^2);
 
 throat_diameter=.021;                                                                     % [m] Diameter of nozzle/throat
-throat_area = (pi*throat_diameter^2);
+throat_area = (pi*(throat_diameter/2)^2);
 
-volume_water_initial = 0;
-mass_water_initial = volume_water_initial * density_water;                                % [kg] Initial mass of water
-volume_initial = volume_bottle;                                                           % Initial volume of air in bottle
+%% Thrust Data
+sample_freq = 1652.00;                  % [Hz]
+
+files = dir('data/model/Group*');
+num_files = length(dir('data/model/Group*'));
+
+for i = 1:num_files
+    
+    %% Read in all files in data/
+    
+    filename = strcat('data/model/',files(i).name);
+    data_temp = load(filename, '-ascii'); % Note: for now all group tests are included
+    data_temp = convforce(data_temp,'lbf','N');
+    
+    data(i).thrust_raw = data_temp(:,3);
+    
+    %% Crop to important sections (TODO)
+    
+    
+    [~,start] = max(data(i).thrust_raw);
+    start = start - 30;
+    if (start < 1)
+        start = 1;
+    end
+    
+    end_data = data(i).thrust_raw(start:end);
+    [~,finish] = min(end_data);
+    
+    finish = finish + start;
+    
+    count = length(start:finish);
+    
+    data_offset = linspace(0,min(data(i).thrust_raw),count)';
+    
+    data(i).thrust = data(i).thrust_raw(start:finish) - data_offset;
+    
+    data(i).time = (1:length(data(i).thrust))./sample_freq;
+    
+    data(i).total_time = (length(data(i).thrust))./sample_freq;
+    
+    
+end
+
+%% Mean Thrust
+mean_thrust = [];
+
+for i = 1:num_files
+    for j = 1:length(data(i).thrust)
+        
+        value = data(i).thrust(j);
+        if (i == 1)
+            mean_thrust(j) = value;
+        else
+            
+            if(length(mean_thrust)<length(data(i).thrust) && j>length(mean_thrust))
+                mean_thrust(j) = 0;
+            else
+                mean_thrust(j) = mean_thrust(j) + value;
+            end
+        end
+    end
+    
+end
+
+thrust_data = mean_thrust ./ num_files;
+
+%% Thermodynamic Model Initial Conditions
+pos_initial = [0 0 0.1];                                                                  % [m] Initial position
+launch_angle = pi/4;                                                                      % [rad] Launch angle
+velocity_initial = [0 0 0];                                                               % [m/s] Initial velocity
+mass_water_initial = volume_water_initial*density_water;                                  % [kg] Initial mass of water
 volume_initial = volume_bottle-volume_water_initial;                                      % Initial volume of air in bottle
-pressure_absolute = pressure_ambient;
 mass_air_initial = (pressure_absolute/(gas_constant*temperature_initial))*volume_initial; % [kg] Initial mass of air
 
 pressure_end = pressure_absolute*(volume_initial/volume_bottle)^1.4;                      % [Pa] Pressure at end of phase one;
@@ -76,37 +140,38 @@ fprintf(fileID,'----- Parameter 1 -----\n');
 fprintf(fileID,'Varying Bottle Mass\n');
 fprintf(fileID,'\nBottle Mass (kg)     Distance Traveled (m)\n\n');
 % Set parameter and fill parameter vector
-bottle_mass = 0.1:0.05:1;
+bottle_mass = 0.1:0.01:1;
 B = bottle_mass;
 
 figure
 
 i = 1;
 % Begin loop. Maybe go make a sandwhich because this can take a while
-for bottle_mass = 0.1:0.05:1;
-
-% Bottle mass sensitivity only
-mass_rocket_initial = mass_water_initial+mass_air_initial+bottle_mass;
-
-vars_init(1) = pos_initial(1);
-vars_init(2) = pos_initial(2);
-vars_init(3) = pos_initial(3);
-vars_init(4) = velocity_initial(1);
-vars_init(5) = velocity_initial(2);
-vars_init(6) = velocity_initial(3);
-vars_init(7) = volume_initial;
-vars_init(8) = mass_rocket_initial;
-vars_init(9) = mass_air_initial;
-fprintf('Parameter 1, Loop Number: %3.0f of %3.0f \n',i,length(B));
-method = 'interpolation';
-[t,vars] = ode45(@(t,vars) eqns(t,vars',method),[0 10], vars_init,ode_options);
-
-A(i) = max(vars(:,1));
-
-fprintf(fileID,'%2.2f                 %5.1f\n',B(i),A(i));
-
-clear vars
-i = i + 1;
+for bottle_mass = 0.1:0.01:1
+    clear vars
+    
+    % Bottle mass sensitivity only
+    mass_rocket_initial = mass_water_initial+mass_air_initial+bottle_mass;
+    
+    vars_init(1) = pos_initial(1);
+    vars_init(2) = pos_initial(2);
+    vars_init(3) = pos_initial(3);
+    vars_init(4) = velocity_initial(1);
+    vars_init(5) = velocity_initial(2);
+    vars_init(6) = velocity_initial(3);
+    vars_init(7) = volume_initial;
+    vars_init(8) = mass_rocket_initial;
+    vars_init(9) = mass_air_initial;
+    fprintf('Parameter 1, Loop Number: %3.0f of %3.0f \n',i,length(B));
+    method = 'interpolation';
+    [t,vars] = ode45(@(t,vars) eqns(t,vars',method),[0 10], vars_init);
+    
+    A(i) = max(vars(:,1));
+    
+    fprintf(fileID,'%2.2f                 %5.1f\n',B(i),A(i));
+    
+    clear vars
+    i = i + 1;
 end
 
 plot(B,A,'b*');
@@ -119,6 +184,7 @@ clear A; clear B;
 % calculations
 bottle_mass = 0.154;
 mass_rocket_initial = mass_water_initial+mass_air_initial+bottle_mass;
+
 %% Parameter 2, Varying Launch Angle
 
 fprintf('\n----- Parameter 2 -----\n');
@@ -126,38 +192,38 @@ fprintf(fileID,'----- Parameter 2 -----\n');
 fprintf(fileID,'Varying Launch Angle\n');
 fprintf(fileID,'\nLaunch Angle (Degrees)     Distance Traveled (m)\n\n');
 % Set parameter and fill parameter vector
-launch_angle_deg = 10:5:75;
+launch_angle_deg = 1:1:90;
 B = launch_angle_deg;
 
 figure
 
 i = 1;
 % Begin loop
-for launch_angle_deg = 10:5:75
+for launch_angle_deg = 1:1:90
+    clear vars
+    % Launch angle conversion, used only for variation of launch angle
+    % sensitivity analysis
+    launch_angle = launch_angle_deg*(pi/180);
     
-% Launch angle conversion, used only for variation of launch angle
-% sensitivity analysis
-launch_angle = launch_angle_deg*(pi/180);
-
-vars_init(1) = pos_initial(1);
-vars_init(2) = pos_initial(2);
-vars_init(3) = pos_initial(3);
-vars_init(4) = velocity_initial(1);
-vars_init(5) = velocity_initial(2);
-vars_init(6) = velocity_initial(3);
-vars_init(7) = volume_initial;
-vars_init(8) = mass_rocket_initial;
-vars_init(9) = mass_air_initial;
-fprintf('Parameter 2, Loop Number: %3.0f of %3.0f \n',i,length(B));
-method = 'interpolation';
-[t,vars] = ode45(@(t,vars) eqns(t,vars',method),[0 10], vars_init,ode_options);
-
-A(i) = max(vars(:,1));
-
-fprintf(fileID,'%2d                         %5.1f\n',B(i),A(i));
-
-clear vars
-i = i + 1;
+    vars_init(1) = pos_initial(1);
+    vars_init(2) = pos_initial(2);
+    vars_init(3) = pos_initial(3);
+    vars_init(4) = velocity_initial(1);
+    vars_init(5) = velocity_initial(2);
+    vars_init(6) = velocity_initial(3);
+    vars_init(7) = volume_initial;
+    vars_init(8) = mass_rocket_initial;
+    vars_init(9) = mass_air_initial;
+    fprintf('Parameter 2, Loop Number: %3.0f of %3.0f \n',i,length(B));
+    method = 'interpolation';
+    [t,vars] = ode45(@(t,vars) eqns(t,vars',method),[0 10], vars_init);
+    
+    A(i) = max(vars(:,1));
+    
+    fprintf(fileID,'%2d                         %5.1f\n',B(i),A(i));
+    
+    clear vars
+    i = i + 1;
 end
 
 plot(B,A,'b*');
@@ -174,34 +240,35 @@ fprintf(fileID,'----- Parameter 3 -----\n');
 fprintf(fileID,'Varying Drag Coefficient\n');
 fprintf(fileID,'\nDrag Coefficient     Distance Traveled (m)\n\n');
 % Set parameter and fill parameter vector
-drag_coeff = 0.1:0.05:1;
+drag_coeff = 0.1:0.01:1;
 B = drag_coeff;
 
 figure
 
 i = 1;
 % Begin loop
-for drag_coeff = 0.1:0.05:1
-
-vars_init(1) = pos_initial(1);
-vars_init(2) = pos_initial(2);
-vars_init(3) = pos_initial(3);
-vars_init(4) = velocity_initial(1);
-vars_init(5) = velocity_initial(2);
-vars_init(6) = velocity_initial(3);
-vars_init(7) = volume_initial;
-vars_init(8) = mass_rocket_initial;
-vars_init(9) = mass_air_initial;
-fprintf('Parameter 3, Loop Number: %3.0f of %3.0f \n',i,length(B));
-method = 'interpolation';
-[t,vars] = ode45(@(t,vars) eqns(t,vars',method),[0 10], vars_init,ode_options);
-
-A(i) = max(vars(:,1));
-
-fprintf(fileID,'%3.3f                %5.1f\n',B(i),A(i));
-
-clear vars
-i = i + 1;
+for drag_coeff = 0.1:0.01:1
+    clear vars
+    
+    vars_init(1) = pos_initial(1);
+    vars_init(2) = pos_initial(2);
+    vars_init(3) = pos_initial(3);
+    vars_init(4) = velocity_initial(1);
+    vars_init(5) = velocity_initial(2);
+    vars_init(6) = velocity_initial(3);
+    vars_init(7) = volume_initial;
+    vars_init(8) = mass_rocket_initial;
+    vars_init(9) = mass_air_initial;
+    fprintf('Parameter 3, Loop Number: %3.0f of %3.0f \n',i,length(B));
+    method = 'interpolation';
+    [t,vars] = ode45(@(t,vars) eqns(t,vars',method),[0 10], vars_init);
+    
+    A(i) = max(vars(:,1));
+    
+    fprintf(fileID,'%3.3f                %5.1f\n',B(i),A(i));
+    
+    clear vars
+    i = i + 1;
 end
 
 plot(B,A,'b*');
@@ -213,20 +280,20 @@ clear A; clear B;
 % Reset used variables to standard values to avoid messing up future
 % calculations
 
-drag_coeff = .3;
+drag_coeff = .2;
 
 % %% Parameter 4
 % fprintf('\n----- Parameter 4 -----\n');
 % % Set parameter and fill parameter vector
 % density_water = 500:50:1500;
 % B = density_water;
-% 
+%
 % figure
-% 
+%
 % i = 1;
 % % Begin loop. Maybe go make a sandwhich because this can take a while
 % for density_water = 500:50:1500
-% 
+%
 % vars_init(1) = pos_initial(1);
 % vars_init(2) = pos_initial(2);
 % vars_init(3) = pos_initial(3);
@@ -239,21 +306,24 @@ drag_coeff = .3;
 % fprintf('Parameter 4, Loop Number: %3.0f of %3.0f \n',i,length(B));
 % method = 'interpolation';
 % [t,vars] = ode45(@(t,vars) eqns(t,vars',method),[0 10], vars_init,ode_options);
-% 
+%
 % A(i) = max(vars(:,1));
-% 
+%
 % clear vars
 % i = i + 1;
 % end
-% 
+%
 % plot(B,A,'b*');
 % title('Varying Propellant Density')
 % xlabel('Propellant Density (kg/m^3)');
 % ylabel('Distance Traveled (m)');
 % clear A; clear B;
-% 
+%
 % density_water = 1000.0;
 
 %% End
+
+fclose(fileID);
+
 TOC = toc;
 fprintf('\n Time to Completion: %3.0f minutes and %2.0f seconds \n',TOC/60,rem(TOC,60))
